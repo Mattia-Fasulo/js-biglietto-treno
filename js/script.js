afea483fbfc3a2ea8bd88b71a559095d648da8a1
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-10-06 17:35:26 +0200

    add exercise

---
+++ js/script.js
@@ -0,0 +1,36 @@
+/**
+x-Chiedere la distanza che si vuole percorrere in km
+x-Chiedere l'età del passegero
+x-Calcolare il prezzo del viaggio (0.21€ per km )
+x-Applicare uno sconto del 20% per gli under 18
+x-Applicare uno sconto del 40% per gli over 65
+x-Calcolare il prezzo finale con massimo due cifre decimali
+-Stampare il biglietto sulla pagina html
+ */
+
+//distanza e età
+let travelDistance =prompt('Inserire la lunghezza del viaggio che si vuole percorrere') ;
+let passengerAge=parseInt(prompt("Inserire l'età del passeggero")) ;
+console.log(travelDistance);
+console.log(passengerAge);
+
+if(isNaN(travelDistance) || isNaN(passengerAge)){
+    alert('inserire un valore numerico');
+}
+
+
+//calcolo il prezzo senza sconti
+let travelPrice = parseFloat((travelDistance * '0.21').toFixed(2));
+console.log(travelPrice);
+
+//applico lo sconto in base all'età
+if(passengerAge <= 18){
+    travelPrice = parseFloat((travelPrice - ((travelPrice) / 5)).toFixed(2));
+}
+else if(passengerAge >= 65){
+    travelPrice = parseFloat((travelPrice - ((travelPrice * 2) / 5)).toFixed(2));
+}
+console.log(travelPrice);
+
+document.getElementById("my-h1").innerHTML = travelPrice + ' ' + '€';
+
